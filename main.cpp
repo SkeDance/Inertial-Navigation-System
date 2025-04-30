f7b19159ca2b81d5616c51938c31074f984c7867
Zaxar Skadi <qoupskd@gmail.com>
2025-04-30 14:03:36 +0300

    parsing data

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 
 #include "include/data.h"
+bool alignmentFlag = false;
 
 int main(){
     data myData;
@@ -12,20 +13,31 @@ int main(){
     std::string path = "20170922_Cessna172_200Hz_Ref.txt";
     std::ifstream Fin;
     Fin.open(path);
-    std::istringstream iss(str);
-    int tokenCount = 0;
-    std:: string token;
+    
     while (std::getline(Fin, str)){
-        ++tokenCount;
-        while(iss >> token)
-        switch(tokenCount){
-            case 1: int t = myData.readTime(token);
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
+        std::istringstream iss(str);
+        int tokenCount = 0;
+        std:: string token;
+        while(iss >> token){
+            switch(tokenCount){
+                case 1: double time = myData.readTime(token);
+                case 2: double gyroX = myData.readGyroX(token);
+                case 3: double gyroY = myData.readGyroY(token);
+                case 4: double gyroZ = myData.readGyroZ(token);
+                case 5: double accelX = myData.readAccelX(token);
+                case 6: double accelY = myData.readAccelY(token);
+                case 7: double accelZ = myData.readAccelZ(token);
+                case 8: double roll_0 = myData.readRoll_0(token);
+                case 9: double pitch_0 = myData.readPitch_0(token);
+                case 10: double yaw_0 = myData.readYaw_0(token);
+                case 13: double fi_0 = myData.readFi_0(token);
+                case 14: double lambda_0 = myData.readLambda_0(token);
+                case 15: double height = myData.readHeight(token);
+            }
         }
+    }
+
+    if(!alignmentFlag){
+        
     }
 }
