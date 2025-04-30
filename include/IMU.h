412ad7d45883d502bee79f69433b82badb526a32
Zaxar Skadi <qoupskd@gmail.com>
2025-04-30 14:13:33 +0300

    update IMU

--- include/IMU.h
+++ include/IMU.h
@@ -1,15 +1,18 @@
 #ifndef IMU_H
 #define IMU_H
 
+#include <cmath>
+#include "../include/math.h"
+
 class IMU{
     public:
-        double d_a = 1 * g * pow(10, -3);      
-        double w_dr = 1 * 0.1 / 3600;           
+        double d_a = 1 * g * pow(10, -3);    
+        double w_dr;           
         
-        double w_M = 0.00020;                    
-        double a_M = 0.00050;                    
+        double w_M;                    
+        double a_M;                    
         
-        double err_Neort = 0.00010;
+        double err_Neort;
 };
 
 #endif
