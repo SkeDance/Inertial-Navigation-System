9cdc015767c34cc8dcfba3e165f21eef8f1a3c6d
Zaxar Skadi <qoupskd@gmail.com>
2025-04-30 13:23:39 +0300

    math

---
+++ include/math.h
@@ -0,0 +1,9 @@
+#ifndef DATA_H
+#define DATA_H
+
+#define PI 3.14159265
+
+double degreesToRads(double degree);
+double radsToDegrees(double rads);
+
+#endif
---
+++ src/math.cpp
@@ -0,0 +1,12 @@
+#include "../include/math.h"
+#include <iostream>
+
+double degreesToRads(double degree)
+{
+    return degree * PI / 180;
+}
+
+double radsToDegrees(double rads)
+{
+    return rads * 180 / PI;
+}
