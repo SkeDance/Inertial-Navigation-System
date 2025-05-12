bfcb9a516348fcf1918672c9fd82f490d7f86bf5
Zaxar Skadi <qoupskd@gmail.com>
2025-05-12 16:52:07 +0300

    add new multiply func

--- include/matrixOperations.h
+++ include/matrixOperations.h
@@ -4,6 +4,8 @@
 class matrix{
     public:
         void multiply(double A[3][1], double B[3][3], double C[3][1]);
+        static void multiply3x3(double A[3][3], double B[3][3], double C[3][3]);
+        void summ(double A[3][1], double B, double[3][1]);
 };
 
 #endif
--- src/matrixOperations.cpp
+++ src/matrixOperations.cpp
@@ -2,15 +2,40 @@
 #include "../include/matrixOperations.h"
 #include "../include/math.h"
 
-int w;
-
-void matrix::multiply(double A[3][1], double B[3][3], double C[3][1]){
-    for (int i = 0; i < 3; i++) {
+void matrix::multiply(double A[3][1], double B[3][3], double C[3][1])
+{
+    for (int i = 0; i < 3; i++)
+    {
         C[i][0] = 0;
-        for (int k = 0; k < 3; k++) {
-            C[i][0] += B[k][0] * A[i][k]; // Суммируем все компоненты
+        for (int k = 0; k < 3; k++)
+        {
+            C[i][0] += B[k][0] * A[i][k];
         }
-        C[i][0] += degreesToRads(w); // Добавляем дрейф после суммирования
     }
 }
+
+void matrix::multiply3x3(double a[3][3], double b[3][3], double result[3][3])
+{
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            result[i][j] = 0;
+            for (int k = 0; k < 3; ++k)
+            {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+void matrix::summ(double A[3][1], double B, double C[3][1])
+{
+    for(int i = 0; i < 3; i++)
+    {
+        C[i][0] += degreesToRads(B);
+    }
+}
+
+
 
