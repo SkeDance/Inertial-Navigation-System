79a9462bca124f6bb27f3f4ec5e15e262d14cbfb
Zaxar Skadi <qoupskd@gmail.com>
2025-05-13 14:03:34 +0300

    update constants

--- include/math.h
+++ include/math.h
@@ -1,9 +1,12 @@
 #ifndef MATH_H
 #define MATH_H
 
-#define PI  3.14159265
-#define g   9.81523
-#define U   15
+#define PI      3.14159265
+#define g       9.81523
+#define U       15
+#define R_Earth 6371000
+#define ECC     0.081819190842622
+#define dt      0.005
 
 double degreesToRads(double degree);
 double radsToDegrees(double rads);
