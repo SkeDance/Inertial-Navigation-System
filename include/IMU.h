17a5801a60a94c02cee1388f178c3fb45888e458
Zaxar Skadi <qoupskd@gmail.com>
2025-05-13 15:12:28 +0300

    IMU navigation functions

--- include/IMU.h
+++ include/IMU.h
@@ -6,7 +6,34 @@
 
 class IMU{
     public:
+        //set
         void set(double roll_0, double pitch_0, double yaw_0, double fi_0);
+        void bodyToLocal(double A[3][3], double B[3][1], double C[3][1]);
+        void calcSpeedVE(double acceleration);
+        void calcSpeedVN(double acceleration);
+        void calcSpeedVUp(double acceleration);
+        void calcAngularWE(double VN);
+        void calcAngularWN(double VE);
+        void calcAngularWUp(double VE);
+        void calcR_fi(double fi);
+        void calcR_lambda(double fi);
+        void calc_aEk();
+        void calc_aNk();
+        void calc_aUpk();
+
+        //get
+        double getSpeedVE();
+        double getSpeedVN();
+        double getSpeedVUp();
+        double getAngularWE();
+        double getAngularWN();
+        double getAngularWUp();
+        double getR_fi();
+        double getR_lambda();
+        double get_aEk();
+        double get_aNk();
+        double get_aUpk();
+
         double d_a = 1 * g * pow(10, -3);    
         double w_dr;           
         
@@ -14,6 +41,15 @@ class IMU{
         double a_M;                    
         
         double err_Neort;
+    private:
+        double VE, VN, VUp;
+        double WE, WN, WUp;
+        double R_fi, R_lambda;
+        double current_aX, previous_aX;
+        double current_aY, previous_aY;
+        double current_aZ, previous_aZ;
+        double aEk, aNk, aUPk;
+
 };
 
 #endif
--- src/IMU.cpp
+++ src/IMU.cpp
@@ -1,5 +1,6 @@
 #include "../include/IMU.h"
 #include "../include/math.h"
+#include "../include/data.h"
 #include "../include/matrixOperations.h"
 
 void IMU::set(double roll_0, double pitch_0, double yaw_0, double fi_0){
@@ -31,4 +32,97 @@ void IMU::set(double roll_0, double pitch_0, double yaw_0, double fi_0){
     CB_PL[2][2] = 1;
 
     matrix::multiply3x3(matrix_0, CB_PL, CB_PL);
+}
+
+void IMU::bodyToLocal(double A[3][3], double B[3][1], double C[3][1]){
+    for (int i = 0; i < 3; i++)
+    {
+        C[i][0] = 0;
+        for (int k = 0; k < 3; k++)
+        {
+            C[i][0] += A[i][k] * B[k][0];
+        }
+    }
+}
+
+void IMU::calcSpeedVE(double acceleration){
+    previous_aX = current_aX;
+    current_aX = acceleration;
+    VE += ((((current_aX + previous_aX) / 2.0) + IMU::get_aEk()) * dt);
+}
+
+void IMU::calcSpeedVN(double acceleration){
+    previous_aY = current_aY;
+    current_aY = acceleration;
+    VN += ((((current_aY + previous_aY) / 2.0) + IMU::get_aNk()) * dt);
+}
+
+void IMU::calcSpeedVUp(double acceleration){
+    previous_aZ = current_aZ;
+    current_aZ = acceleration;
+    VUp += ((((current_aZ + previous_aZ) / 2.0) + IMU::get_aUpk()) * dt);
+}
+
+void IMU::calcR_fi(double fi){
+    R_fi = (R_Earth * (1 - pow(ECC, 2))) / pow(1 - pow(ECC, 2) * pow(sin(fi), 2), 3.0 / 2.0);
+}
+
+void IMU::calcR_lambda(double fi){
+    R_lambda = (R_Earth) / pow(1 - pow(ECC, 2) * pow(sin(fi), 2), 1.0 / 2.0);
+}
+
+void IMU::calcAngularWE(double VN){
+    WE = (-VN / (getR_fi() + data::getHeight()));
+}
+
+void  IMU::calcAngularWN(double VE){
+    WN = (VE / (getR_lambda() + data::getFi_0()) + U * cos(data::getFi_0()));
+}
+
+void IMU::calcAngularWUp(double VE){
+    WUp = (VE / (getR_lambda() + data::getHeight()) * tan(data::getFi_0()) + U * sin(data::getFi_0()));
+}
+
+double IMU::getAngularWE(){
+    return WE;
+}
+
+double IMU::getAngularWN(){
+    return WN;
+}
+
+double IMU::getAngularWUp(){
+    return WUp;
+}
+
+void IMU::calc_aEk(){
+    aEk = ((-IMU::getAngularWN() * IMU::getSpeedVUp()) + (IMU::getAngularWUp() * IMU::getSpeedVN()) +(-U * cos(data::getFi_0()) * IMU::getSpeedVUp()) + (U * sin(data::getFi_0()) * IMU::getSpeedVN()));
+}
+
+void IMU::calc_aNk(){
+    aNk = ((-IMU::getAngularWE() * IMU::getSpeedVUp()) + (-IMU::getAngularWUp() * IMU::getSpeedVE()) + (-U * sin(data::getFi_0()) * IMU::getSpeedVE()));
+}
+
+void IMU::calc_aUpk(){
+    aUPk = ((-IMU::getAngularWE() * IMU::getSpeedVN()) + (IMU::getAngularWN() * IMU::getSpeedVE()) + (U * cos(data::getFi_0()) * IMU::getSpeedVE() - g));
+}
+
+double IMU::get_aEk(){
+    return aEk;
+}
+
+double IMU::get_aNk(){
+    return aNk;
+}
+
+double IMU::get_aUpk(){
+    return aUPk;
+}
+
+double IMU::getR_fi(){
+    return R_fi;
+}
+
+double IMU::getR_lambda(){
+    return R_lambda;
 }
