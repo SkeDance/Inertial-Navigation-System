194c2cccd922ea63a7ad1b2e66397885d06aa821
Zaxar Skadi <qoupskd@gmail.com>
2025-05-13 16:13:08 +0300

    new matrix functions

--- src/matrixOperations.cpp
+++ src/matrixOperations.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include "../include/matrixOperations.h"
 #include "../include/math.h"
 
@@ -34,6 +35,68 @@ void matrix::summ(double A[3][1], double B, double C[3][1])
     for(int i = 0; i < 3; i++)
     {
         C[i][0] += degreesToRads(B);
+    }
+}
+
+void matrix::Poisson(double A[3][3], double B[3][3], double C[3][3]){
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            C[i][j] = 0;
+            for (int k = 0; k < 3; ++k)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void matrix::subtractMatrix(double A[3][3], double B[3][3], double C[3][3]){
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            C[i][j] = A[i][j] - B[i][j];
+        }
+    }
+}
+
+void matrix::updateMatrix(double LL[3][3], double LL_dt[3][3]){
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            LL[i][j] += LL_dt[i][j] * dt;
+        }
+    }
+}
+
+void matrix::normalizeMatrix(double C_B_LL[3][3], int t){
+    for (int i = 0; i < 3; i++)
+    {
+        double s;
+
+        if (t % 2 == 0)
+        { // Если такт четный, нормализация по строкам
+            s = sqrt(C_B_LL[i][0] * C_B_LL[i][0] +
+                     C_B_LL[i][1] * C_B_LL[i][1] +
+                     C_B_LL[i][2] * C_B_LL[i][2]);
+
+            C_B_LL[i][0] /= s;
+            C_B_LL[i][1] /= s;
+            C_B_LL[i][2] /= s;
+        }
+        else
+        { // Если такт нечетный, нормализация по столбцам
+            s = sqrt(C_B_LL[0][i] * C_B_LL[0][i] +
+                     C_B_LL[1][i] * C_B_LL[1][i] +
+                     C_B_LL[2][i] * C_B_LL[2][i]);
+
+            C_B_LL[0][i] /= s;
+            C_B_LL[1][i] /= s;
+            C_B_LL[2][i] /= s;
+        }
     }
 }
 
