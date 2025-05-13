7a95bbb93a9402ade51ca70708cff763f9e0cb02
Zaxar Skadi <qoupskd@gmail.com>
2025-05-13 14:05:39 +0300

    rework parsing

--- include/data.h
+++ include/data.h
@@ -1,22 +1,33 @@
 #ifndef DATA_H
 #define DATA_H
+
 #include <string>
+#include <sstream>
 
 class data{
+    private:
+        static double time;
+        static double gyroX, gyroY, gyroZ;
+        static double accelX, accelY, accelZ;
+        static double height;
+        static double roll_0, pitch_0, yaw_0;
+        static double fi_0, lambda_0;
     public:
-        double readTime(std::string token);
-        double readGyroX(std::string token);
-        double readGyroY(std::string token);
-        double readGyroZ(std::string token);
-        double readAccelX(std::string token);
-        double readAccelY(std::string token);
-        double readAccelZ(std::string token);
-        double readHeight(std::string token);
-        double readRoll_0(std::string token);
-        double readPitch_0(std::string token);
-        double readYaw_0(std::string token);
-        double readFi_0(std::string token);
-        double readLambda_0(std::string token);
+        void parse(const std::string& str);
+
+        static double getTime();
+        static double getGyroX();
+        static double getGyroY();
+        static double getGyroZ();
+        static double getAccelX();
+        static double getAccelY();
+        static double getAccelZ();
+        static double getHeight();
+        static double getRoll_0();
+        static double getPitch_0();
+        static double getYaw_0();
+        static double getFi_0();
+        static double getLambda_0();
 };
 
 #endif
--- src/data.cpp
+++ src/data.cpp
@@ -1,59 +1,107 @@
 #include "../include/data.h"
 #include <iostream>
 
+void data::parse(const std::string& str) {
+    std::istringstream iss(str);
+    std::string token;
+    int tokenCount = 0;
 
-double data::readTime(std::string token){
-    return std::stof(token);
+    while (iss >> token) {
+        switch (tokenCount) {
+            case 1:  // time (предполагаем, что токены нумеруются с 0)
+                time = std::stod(token);
+                break;
+            case 2:  // gyroX
+                gyroX = std::stod(token);
+                break;
+            case 3:  // gyroY
+                gyroY = std::stod(token);
+                break;
+            case 4:  // gyroZ
+                gyroZ = std::stod(token);
+                break;
+            case 5:  // accelX
+                accelX = std::stod(token);
+                break;
+            case 6:  // accelY
+                accelY = std::stod(token);
+                break;
+            case 7:  // accelZ
+                accelZ = std::stod(token);
+                break;
+            case 8:  // roll_0
+                roll_0 = std::stod(token);
+                break;
+            case 9:  // pitch_0
+                pitch_0 = std::stod(token);
+                break;
+            case 10: // yaw_0
+                yaw_0 = std::stod(token);
+                break;
+            case 13: // fi_0
+                fi_0 = std::stod(token);
+                break;
+            case 14: // lambda_0
+                lambda_0 = std::stod(token);
+                break;
+            case 15: // height
+                height = std::stod(token);
+                break;
+            default:
+                // Игнорируем ненужные токены
+                break;
+        }
+        tokenCount++;
+    }
+}
+double data::getTime(){
+    return time;
 }
 
-double data::readGyroX(std::string token){
-    return std::stof(token);
+double data::getGyroX(){
+    return gyroX;
 }
 
-double data::readGyroY(std::string token){
-    return std::stof(token);
+double data::getGyroY(){
+    return gyroY;
 }
 
-double data::readGyroZ(std::string token){
-    return std::stof(token);
+double data::getGyroZ(){
+    return gyroZ;
 }
 
-double data::readAccelX(std::string token){
-    return std::stof(token);
+double data::getAccelX(){
+    return accelX;
 }
 
-double data::readAccelY(std::string token){
-    return std::stof(token);
+double data::getAccelY(){
+    return accelY;
 }
 
-double data::readAccelZ(std::string token){
-    return std::stof(token);
+double data::getAccelZ(){
+    return accelZ;
 }
 
-double data::readHeight(std::string token){
-    return std::stof(token);
+double data::getHeight(){
+    return height;
 }
 
-double data::readHeight(std::string token){
-    return std::stof(token);
+double data::getRoll_0(){
+    return roll_0;
 }
 
-double data::readRoll_0(std::string token){
-    return std::stof(token);
+double data::getPitch_0(){
+    return pitch_0;
 }
 
-double data::readPitch_0(std::string token){
-    return std::stof(token);
+double data::getYaw_0(){
+    return yaw_0;
 }
 
-double data::readYaw_0(std::string token){
-    return std::stof(token);
+double data::getFi_0(){
+    return fi_0;
 }
 
-double data::readFi_0(std::string token){
-    return std::stof(token);
-}
-
-double data::readLambda_0(std::string token){
-    return std::stof(token);
+double data::getLambda_0(){
+    return lambda_0;
 }
