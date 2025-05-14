e2008caa3a4469dc937ca4bf74247c657f50d9e4
Zaxar Skadi <qoupskd@gmail.com>
2025-05-14 15:44:28 +0300

    add static and temp arrays

--- include/matrixOperations.h
+++ include/matrixOperations.h
@@ -3,13 +3,13 @@
 
 class matrix{
     public:
-        void multiply(double A[3][1], double B[3][3], double C[3][1]);
+        static void multiply(double A[3][3], double B[3][1], double C[3][1]);
         static void multiply3x3(double A[3][3], double B[3][3], double C[3][3]);
         static void Poisson(double A[3][3], double B[3][3], double C[3][3]);
         static void subtractMatrix(double A[3][3], double B[3][3], double C[3][3]);
         static void updateMatrix(double LL[3][3], double LL_dt[3][3]);
         static void normalizeMatrix(double C_B_LL[3][3], int t);
-        void summ(double A[3][1], double B, double[3][1]);
+        static void summ(double A[3][1], double B, double[3][1]);
 };
 
 #endif
--- src/matrixOperations.cpp
+++ src/matrixOperations.cpp
@@ -3,29 +3,39 @@
 #include "../include/matrixOperations.h"
 #include "../include/math.h"
 
-void matrix::multiply(double A[3][1], double B[3][3], double C[3][1])
-{
-    for (int i = 0; i < 3; i++)
-    {
-        C[i][0] = 0;
-        for (int k = 0; k < 3; k++)
-        {
-            C[i][0] += B[k][0] * A[i][k];
+void matrix::multiply(double A[3][3], double B[3][1], double C[3][1]){
+    double temp[3][1] = {0};
+    for (int i = 0; i < 3; i++){
+        for (int k = 0; k < 3; k++){
+            temp[i][0] += B[k][0] * A[i][k];
         }
+    }
+    for (int i = 0; i < 3; i++){
+        C[i][0] = temp[i][0];
     }
 }
 
 void matrix::multiply3x3(double a[3][3], double b[3][3], double result[3][3])
 {
+    double temp[3][3] = {0}; 
+
     for (int i = 0; i < 3; ++i)
     {
         for (int j = 0; j < 3; ++j)
         {
-            result[i][j] = 0;
+            temp[i][j] = 0;
             for (int k = 0; k < 3; ++k)
             {
-                result[i][j] += a[i][k] * b[k][j];
+                temp[i][j] += a[i][k] * b[k][j];
             }
+        }
+    }
+    
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            result[i][j] = temp[i][j];
         }
     }
 }
